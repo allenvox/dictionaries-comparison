15fbb49e4c299cd07a14195cd337066a72cefa22
Yuriy Grigoryev <allenvox@MacBook-Pro-Yuriy.local>
2022-04-01 14:05:24 +0700

    Add hash tables library

---
+++ hashtab.h
@@ -0,0 +1,13 @@
+#pragma once
+
+struct listnode {
+  char *key;
+  int value;
+  struct listnode *next;
+};
+
+unsigned int hashtab_hash(char *key);
+void hashtab_init(struct listnode **hashtab);
+void hashtab_add(struct listnode **hashtab, char *key, int value);
+struct listnode *hashtab_lookup(struct listnode **hashtab, char *key);
+void hashtab_delete(struct listnode **hashtab, char *key);

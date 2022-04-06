8e8977fc78ca62eb48e2fde5a87ce2499933abfa
Yuriy Grigoryev <allenvox@MacBook-Pro-Yuriy.local>
2022-04-06 15:12:54 +0700

    Include collision handling in hashtab lib

--- hashtab.c
+++ hashtab.c
@@ -3,47 +3,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define HASHTAB_SIZE 5051
+unsigned int hash(char* key, hashtype type, int size){
+    if(type == KR) {
+        return KR_hash(key, size);
+    } else {
+        return FNV_hash(key, size);
+    }
+}
 
-struct listnode *hashtab[HASHTAB_SIZE];
+unsigned int FNV_hash(char *key, int size) {
+  unsigned int h = 0x811c9dc5;
+  while(*key) {
+    h ^= (unsigned int)*key++;
+    h *= 0x01000193;
+  }
+  return h % size;
+}
 
-unsigned int hashtab_hash(char *key) {
+unsigned int KR_hash(char *key, int size) {
   unsigned int h = 0, hash_mul = 31;
   while (*key)
     h = h * hash_mul + (unsigned int)*key++;
-  return h % HASHTAB_SIZE;
+  return h % size;
 }
 
-void hashtab_init(struct listnode **hashtab) {
-  for (int i = 0; i < HASHTAB_SIZE; i++)
+void hashtab_init(listnode **hashtab, int size) {
+  for (int i = 0; i < size; i++)
     hashtab[i] = NULL;
 }
 
-void hashtab_add(struct listnode **hashtab, char *key, int value) {
-  struct listnode *node;
-  int index = hashtab_hash(key);
-  node = malloc(sizeof(*node));
-  if (node != NULL) {
+void hashtab_add(listnode **hashtab, char *key, int value, hashtype type, int size, int *collision) {
+  int index = hash(key, type, size);
+  listnode *node = malloc(sizeof(*node));
+  if(node != NULL) { // malloc succeeded
     node->key = key;
     node->value = value;
-    node->next = hashtab[index];
-    hashtab[index] = node;
+    if(hashtab[index] != NULL) {
+      *collision += 1;
+      node->next = hashtab[index]->next;
+      hashtab[index]->next = node;
+    } else {
+      node->next = hashtab[index];
+      hashtab[index] = node;
+    }
+  } else {
+    printf("Error while allocating memory\n");
   }
 }
 
-struct listnode *hashtab_lookup(struct listnode **hashtab, char *key) {
-  struct listnode *node;
-  int index = hashtab_hash(key);
+listnode *hashtab_lookup(listnode **hashtab, char *key, hashtype type, int size) {
+  listnode *node;
+  int index = hash(key, type, size);
   for (node = hashtab[index]; node != NULL; node = node->next) {
-    if (0 == strcmp(node->key, key))
+    if (0 == strcmp(node->key, key)) {
       return node;
+    }
   }
   return NULL;
 }
 
-void hashtab_delete(struct listnode **hashtab, char *key) {
-  struct listnode *node, *prev = NULL;
-  int index = hashtab_hash(key);
+void hashtab_delete(listnode **hashtab, char *key, hashtype type, int size) {
+  listnode *node, *prev = NULL;
+  int index = hash(key, type, size);
   for (node = hashtab[index]; node != NULL; node = node->next) {
     if (0 == strcmp(node->key, key)) {
       if (prev == NULL)
@@ -55,4 +76,49 @@ void hashtab_delete(struct listnode **hashtab, char *key) {
     }
     prev = node;
   }
+}
+
+listnode *list_createnode(char *key, int value) {
+  listnode *p = malloc(sizeof(*p));
+  if (p != NULL) {
+    p->key = key;
+    p->value = value;
+    p->next = NULL;
+  }
+  return p;
+}
+
+listnode *list_addfront(listnode *list, char *key, int value) {
+  listnode *newnode = list_createnode(key, value);
+  if (newnode != NULL) {
+    newnode->next = list;
+    return newnode;
+  }
+  return list;
+}
+
+listnode *list_lookup(listnode *list, char *key) {
+  for (; list != NULL; list = list->next) {
+    if (strcmp(list->key, key) == 0) {
+      return list;
+    }
+  }
+  return NULL; // if not found
+}
+
+listnode *list_delete(listnode *list, char *key) {
+  listnode *p, *prev = NULL;
+  for (p = list; p != NULL; p = p->next) {
+    if (strcmp(p->key, key) == 0) {
+      if (prev == NULL) {
+        list = p->next; /* Удаляем голову */
+      } else {
+        prev->next = p->next; /* Есть элемент слева */
+        free(p); /* Освобождаем память */
+        return list; /* Указатель на новую голову */
+      }
+    }
+    prev = p; /* Запоминаем предыдущий элемент */
+  }
+  return NULL; // not found
 }
--- hashtab.h
+++ hashtab.h
@@ -1,13 +1,21 @@
 #pragma once
 
-struct listnode {
+typedef struct listnode {
   char *key;
   int value;
   struct listnode *next;
-};
+} listnode;
 
-unsigned int hashtab_hash(char *key);
-void hashtab_init(struct listnode **hashtab);
-void hashtab_add(struct listnode **hashtab, char *key, int value);
-struct listnode *hashtab_lookup(struct listnode **hashtab, char *key);
-void hashtab_delete(struct listnode **hashtab, char *key);
+typedef enum hashtype { KR, FNV } hashtype;
+
+unsigned int hash(char *key, hashtype type, int size);
+unsigned int FNV_hash(char *key, int size);
+unsigned int KR_hash(char *key, int size);
+void hashtab_init(listnode **hashtab, int size);
+void hashtab_add(listnode **hashtab, char *key, int value, hashtype type, int size, int *collision);
+listnode *hashtab_lookup(listnode **hashtab, char *key, hashtype type, int size);
+void hashtab_delete(listnode **hashtab, char *key, hashtype type, int size);
+listnode *list_createnode(char *key, int value);
+listnode *list_addfront(listnode *list, char *key, int value);
+listnode *list_lookup(listnode *list, char *key);
+listnode *list_delete(listnode *list, char *key);
